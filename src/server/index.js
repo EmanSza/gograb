e1f134aa00dec68ed90beece488e55e62f0c4e16
EmanSza <eszabadka@hotmail.ca>
2023-09-30 12:02:55 -0400

    Cleaned up Index
    Moved Electron to its own file

--- src/server/index.js
+++ src/server/index.js
@@ -1,38 +1,9 @@
-const {app, BrowserWindow} = require('electron');
-const path = require('path');
 const server = require('./server');
+const startElectron = require('./utility/electron');
 
 
 server.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
 
-function createWindow () {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
-    })
-
-    win.loadURL('http://localhost:3000')
-}
-
-
-
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+startElectron();
--- src/server/utility/electron.js
+++ src/server/utility/electron.js
@@ -1,35 +1,36 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 
-const express = require('express');
-const flash = require('express-flash');
-const exapp = express();
-
-exapp.use(express.static(__dirname + '../client/public'));
-exapp.set('views', __dirname + '../client/pages');
-exapp.set('view engine', 'ejs');
-exapp.use(express.urlencoded({ extended: true }));
-exapp.use(express.json());
-exapp.use(flash());
-
-const server = exapp.listen(3000, () => {
-    console.log('Server listening on port 3000');
-});
-
-const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-        preload: path.join(__dirname, '../utility/preload.js')
+const startElectron = () => {
+    function createWindow () {
+        const win = new BrowserWindow({
+          width: 800,
+          height: 600,
+          webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+          }
+        })
+    
+        win.loadURL('http://localhost:3000')
     }
-});
+    
+    
+    
+    app.whenReady().then(() => {
+      createWindow()
+    
+      app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+          createWindow()
+        }
+      })
+    })
+    
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    })
+}
 
-mainWindow.loadURL('http://localhost:3000');
-
-app.on('window-all-closed', () => {
-    server.close();
-    app.quit();
-});
-
-// Path: src/server/utility/preload.js
-
+module.exports = startElectron;
